8f4642dbb129d811376ed4a6075bfc0d394868e4
Prathamesh-Srivastava <prathameshsrivastava04@gmail.com>
2024-06-18 16:25:26 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require('dotenv');
 const cookieParser = require("cookie-parser");
@@ -11,6 +12,9 @@ const connectToDb = require("./DB/connectToDb");
 
 const { app, server } = require("./socket/socket");
 
+//for deployment
+const dirname = path.resolve();
+
 dotenv.config();
 app.use(cors({
     origin: "http://localhost:3000",
@@ -20,11 +24,17 @@ app.use(express.json());
 app.use(cookieParser());
 
 
-const Port = process.env.PORT || 8000;
+const Port = process.env.PORT || 5000;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 app.use("/api/users", userRoutes);
+
+//for deployment
+app.use(express.static(path.join(dirname, "/frontend/build")));
+app.get("*", (req,res) => {
+    res.sendFile(path.join(dirname, "frontend", "build", "index.html"));
+});
 
 server.listen(Port,()=> {
     connectToDb()
